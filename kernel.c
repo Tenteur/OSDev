e287b0afbb0ef62c2a66f88379d1e9f3910c2ecf
Tenteur592 <vianneyleman592@gmail.com>
2022-09-02 14:59:24 +0200

    corrigé

--- kernel.c
+++ kernel.c
@@ -121,5 +121,5 @@ void kernel_main(void)
 
     /* Newline support is left as an exercise. */
     terminal_writestring("Hello, kernel World!\n");
-    terminal_writestring("doeas this worked ?")
+    terminal_writestring("doeas this worked ?");
 }
