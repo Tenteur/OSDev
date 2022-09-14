c774e8a81d6c6f2f28e77379c8d6e5151cd00241
Tenteur592 <vianneyleman592@gmail.com>
2022-09-14 21:07:28 +0200

    nouvelle mise a jour !

--- kernel/arch/i386/tty.c
+++ kernel/arch/i386/tty.c
@@ -46,11 +46,11 @@ void terminal_putchar(char c) {
 		if (++terminal_row == VGA_HEIGHT)
 			terminal_row = 0;
 	}
-	if (c == '\n'){
+	if (str[c] == str['\n']){
 		terminal_row = terminal_row + 1;
 		terminal_column = 0;
 	}
-	if (c = '\fb'){
+	if (str[c] = str['\fb']){
 			terminal_setcolor(VGA_COLOR_BLUE);
 	}
 }
