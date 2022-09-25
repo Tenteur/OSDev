cce662b0b1e3cf9228c984c89cc07f6455ddc3df
Tenteur592 <vianneyleman592@gmail.com>
2022-09-25 20:07:35 +0200

    correction

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -6,7 +6,7 @@ void kernel_main(void) {
 	terminal_initialize();
 	printf("Hello, kernel World!\n");
 	printf("this is now a new line !\n is this a newline ?\n");
-	change_color(1)
+	change_color(1);
 	printf("now change the color !");
 	//printf(inportb(0x64));
 	//printf(inportb(0x60));
