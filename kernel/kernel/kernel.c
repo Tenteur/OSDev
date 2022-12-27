0647a5798fafe756788cb5ffedbfbe25c5bd00c9
Tenteur592 <vianneyleman592@gmail.com>
2022-12-27 19:07:14 +0100

    rétablie ancien fichier et modifié MakeFile dans kernel.

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -9,17 +9,16 @@
 #include "../include/kernel/timer.h"
 #include "../include/kernel/drivers/keyboard.h"
 
-void start_kernel(void) {
+void start_kernel() {
 	printf("Installing interrupt service routines (ISRs).\n");
+	isr_install();
 }
 
 void kernel_main(void) {
-	isr_install();
 	terminal_initialize();
 	printf("Hello kernel world!\n");
 	change_color(1, 3);
 	printf("now change the color !");
-	execute_command();
 }
 
 void execute_command(char* input)
